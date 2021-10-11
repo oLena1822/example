d0a8863ecc5b24420b1388bc059e3fb3aad75feb
Tomasz Babczyński <tomasz.babczynski@pwr.edu.pl>
2021-10-11 12:28:54 +0200

    dodane zmienne i format

--- src/tb/soft/Main.java
+++ src/tb/soft/Main.java
@@ -3,8 +3,13 @@ package tb.soft;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Witaj");
+        String lancuch, imie;
 
-    System.exit(0);
+        lancuch="Witaj";
+        imie="Tomek";
+
+	    System.out.println(String.format("%s posiadaczu imienia: %s",lancuch, imie));
+
+        System.exit(0);
     }
 }
